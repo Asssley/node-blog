de3eb20d63e1088b02d94409c921f12220c52dc2
Asssley <turbinnazar@.com>
2025-06-23 18:25:23 +0300

    add basic html

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,32 @@
-import express, {Express} from "express";
+import express, { Express } from "express";
+import morgan from "morgan";
 import { createPath } from "./utils/createPath";
 
 const PORT = 3000;
 
-const app:Express = express();
-
-console.log(createPath(""));
+const app: Express = express();
 
 app.use(express.static("static"));
 
+app.use(express.urlencoded({ extended: false }))
+
+app.use(morgan(':method :url :status - :response-time ms'));
+
 app.get("/", (req, res) => {
-  res.sendFile(createPath("index.html"));
-})
+  res.sendFile(createPath("index"));
+});
+
+app.get('/favicon.ico', (req, res) => {
+  res.status(204).end();
+});
+
+app.get('/.well-known/appspecific/com.chrome.devtools.json', (req, res) => {
+  res.status(204).end();
+});
+
 
 app.use((req, res) => {
-  res.sendFile(createPath("errorPage,"));
-})
+  res.sendFile(createPath("errorPage"));
+});
 
-app.listen(PORT, (error) => error ? console.log(error): console.log(`Server listening port ${PORT}`));
+app.listen(PORT, (error) => error ? console.log(error) : console.log(`Server listening port ${PORT}`));
